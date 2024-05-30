68ccdb662a5c2eaf329828c2121af9e56132383e
Tim Larsson <timakelarsson@gmail.com>
2024-05-30 10:47:01 +0200

    Fixed restart script

--- restart.sh
+++ restart.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-kill $(pgrep -f "python $SCRIPT_DIR/dbus-shelly-3em-pvinverter.py")
+# Get the process ID(s) of the running script
+PIDS=$(pgrep -f "python $SCRIPT_DIR/dbus-shelly-3em-pvinverter.py")
+
+# Check if any process IDs were found
+if [ -z "$PIDS" ]; then
+  echo "No running process found for dbus-shelly-3em-pvinverter.py"
+else
+  echo "Killing process ID(s): $PIDS"
+  # Kill the process ID(s)
+  kill $PIDS
+fi
+
+# Start the script again
+echo "Starting dbus-shelly-3em-pvinverter.py script..."
+python $SCRIPT_DIR/dbus-shelly-3em-pvinverter.py &
